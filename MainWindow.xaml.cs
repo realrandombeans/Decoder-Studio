917387f5a87ffc2f6ffc7723f0be2f22fee3fce6
randombeans <olliereynolds10@gmail.com>
2023-08-22 22:20:27 +0100

    Lots of Additions

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,6 +5,8 @@ using System.IO;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using System.Windows.Controls;
+using System.Linq;
 
 namespace StudioDecoder
 {
@@ -77,6 +79,7 @@ namespace StudioDecoder
             {
                 string filePath = openFileDialog.FileName;
                 logTextBox.AppendText("Loading File - " + openFileDialog.FileName + Environment.NewLine);
+                logTextBox.ScrollToEnd();
 
                 if (VideoCheckbox.IsChecked == true)
                 {
@@ -92,6 +95,7 @@ namespace StudioDecoder
                     if (videoresult == true)
                     {
                         logTextBox.AppendText("Loading Video - " + openFileVideoDialog.FileName + Environment.NewLine);
+                        logTextBox.ScrollToEnd();
                         VideoPlayer.Source = new Uri(openFileVideoDialog.FileName, UriKind.Absolute);
                     }
                 }
@@ -106,11 +110,17 @@ namespace StudioDecoder
             using (StreamReader reader = new StreamReader(filePath))
             {
                 int lineNumber = 0;
+                int totalLines = File.ReadLines(filePath).Count(); // Count total lines in the file
                 string line;
 
                 while ((line = reader.ReadLine()) != null && isProcessing)
                 {
                     lineNumber++;
+                    // Calculate progress percentage
+                    double progress = (double)lineNumber / totalLines * 100;
+
+                    // Update the progress bar on the UI thread
+                    Dispatcher.Invoke(() => ShowtapeProgressBar.Value = progress);
 
                     // Ignore the first three characters
                     string frameData = line.Substring(3);
@@ -163,8 +173,6 @@ namespace StudioDecoder
                             isNonFrameData = false;
                             nonFrameData = "";
                         }
-
-                        // Delay for frame rate
                         double intervalInSeconds = 1.0 / 30.0;
                         await Task.Delay(TimeSpan.FromSeconds(intervalInSeconds));
                     }
@@ -189,10 +197,12 @@ namespace StudioDecoder
                 {
                     string logMessage = $"Analog Command. Channel {channel}, Value {value}.";
                     logTextBox.AppendText(logMessage + Environment.NewLine);
+                    logTextBox.ScrollToEnd();
                 }
                 else
                 {
                     logTextBox.AppendText($"Unknown command: {command}" + Environment.NewLine);
+                    logTextBox.ScrollToEnd();
                 }
             }
             else if (command == "E")
@@ -209,10 +219,12 @@ namespace StudioDecoder
                 {
                     string logMessage = $"Disk Command. Disk {disk}, Time {timestamp1}{timestamp2}.";
                     logTextBox.AppendText(logMessage + Environment.NewLine);
+                    logTextBox.ScrollToEnd();
                 }
                 else
                 {
                     logTextBox.AppendText($"Unknown command: {command}" + Environment.NewLine);
+                    logTextBox.ScrollToEnd();
                 }
             }
             else if (command.StartsWith("M211") && command.Contains("V"))
@@ -226,17 +238,20 @@ namespace StudioDecoder
                         string outputName = GetVideoOutputName(output);
                         string logMessage = $"Video Command Unknown displaying to {outputName}.";
                         logTextBox.AppendText(logMessage + Environment.NewLine);
+                        logTextBox.ScrollToEnd();
                     }
                     else
                     {
                         string outputName = GetVideoOutputName(output);
                         string logMessage = $"Video Command. Disk {disk}, Output {outputName}.";
                         logTextBox.AppendText(logMessage + Environment.NewLine);
+                        logTextBox.ScrollToEnd();
                     }
                 }
                 else
                 {
                     logTextBox.AppendText($"Unknown command: {command}" + Environment.NewLine);
+                    logTextBox.ScrollToEnd();
                 }
             }
             else if (command.StartsWith("M212") && command.Contains("V"))
@@ -247,10 +262,12 @@ namespace StudioDecoder
                 {
                     string logMessage = GetAudioCommandMessage(output, info);
                     logTextBox.AppendText(logMessage + Environment.NewLine);
+                    logTextBox.ScrollToEnd();
                 }
                 else
                 {
                     logTextBox.AppendText($"Unknown command: {command}" + Environment.NewLine);
+                    logTextBox.ScrollToEnd();
                 }
             }
             else if (command.StartsWith("M206") && command.Contains("V"))
@@ -261,15 +278,18 @@ namespace StudioDecoder
                 {
                     string logMessage = $"Fade Disk {disk} to {fadeToValue} at Rate: {rate}.";
                     logTextBox.AppendText(logMessage + Environment.NewLine);
+                    logTextBox.ScrollToEnd();
                 }
                 else
                 {
                     logTextBox.AppendText($"Unknown command: {command}" + Environment.NewLine);
+                    logTextBox.ScrollToEnd();
                 }
             }
             else
             {
                 logTextBox.AppendText($"Unknown command: {command}" + Environment.NewLine);
+                logTextBox.ScrollToEnd();
             }
         }
 
@@ -363,47 +383,56 @@ namespace StudioDecoder
                         {
                             logMessage = $"Bit value for '{character}' is: {bit} on DTU 1 and has been disabled";
                             logTextBox.AppendText(logMessage + Environment.NewLine);
+                            logTextBox.ScrollToEnd();
                         }
                         else if (nonFrameData[0] == '1')
                         {
                             logMessage = $"Bit value for '{character}' is: {bit} on DTU 1 and has been enabled";
                             logTextBox.AppendText(logMessage + Environment.NewLine);
+                            logTextBox.ScrollToEnd();
                         }
                         if (nonFrameData[0] == '2')
                         {
                             logMessage = $"Bit value for '{character}' is: {bit + 32} on DTU 1 and has been disabled";
                             logTextBox.AppendText(logMessage + Environment.NewLine);
+                            logTextBox.ScrollToEnd();
                         }
                         else if (nonFrameData[0] == '3')
                         {
                             logMessage = $"Bit value for '{character}' is: {bit + 32} on DTU 1 and has been enabled";
                             logTextBox.AppendText(logMessage + Environment.NewLine);
+                            logTextBox.ScrollToEnd();
                         }
                         if (nonFrameData[0] == '4')
                         {
                             logMessage = $"Bit value for '{character}' is: {bit} on DTU 2 and has been disabled";
                             logTextBox.AppendText(logMessage + Environment.NewLine);
+                            logTextBox.ScrollToEnd();
                         }
                         else if (nonFrameData[0] == '5')
                         {
                             logMessage = $"Bit value for '{character}' is: {bit} on DTU 2 and has been enabled";
                             logTextBox.AppendText(logMessage + Environment.NewLine);
+                            logTextBox.ScrollToEnd();
                         }
                         if (nonFrameData[0] == '6')
                         {
                             logMessage = $"Bit value for '{character}' is: {bit + 32} on DTU 2 and has been disabled";
                             logTextBox.AppendText(logMessage + Environment.NewLine);
+                            logTextBox.ScrollToEnd();
                         }
                         else if (nonFrameData[0] == '7')
                         {
                             logMessage = $"Bit value for '{character}' is: {bit + 32} on DTU 2 and has been enabled";
                             logTextBox.AppendText(logMessage + Environment.NewLine);
+                            logTextBox.ScrollToEnd();
                         }
                     }
                     else
                     {
                         // Append the error message to the TextBox
                         logTextBox.AppendText($"No bit mapping found for character '{character}'." + Environment.NewLine);
+                        logTextBox.ScrollToEnd();
                     }
                 }
             }
@@ -411,6 +440,7 @@ namespace StudioDecoder
             {
                 // Append the error message to the TextBox
                 logTextBox.AppendText($"An error occurred: {ex.Message}" + Environment.NewLine);
+                logTextBox.ScrollToEnd();
             }
         }
 
@@ -418,35 +448,103 @@ namespace StudioDecoder
         public void StopProcessing()
         {
             isProcessing = false;
+            ShowtapeProgressBar.Value = 0;
         }
 
         private void Pause_Click(object sender, RoutedEventArgs e)
         {
             logTextBox.AppendText("Paused Processing" + Environment.NewLine);
+            logTextBox.ScrollToEnd();
         }
 
         private void Play_Click(object sender, RoutedEventArgs e)
         {
             logTextBox.AppendText("Unpaused Processing" + Environment.NewLine);
+            logTextBox.ScrollToEnd();
         }
 
         private void Stop_Click(object sender, RoutedEventArgs e)
         {
             StopProcessing();
             logTextBox.AppendText("Cancellled Processing" + Environment.NewLine);
+            logTextBox.ScrollToEnd();
         }
         private void Forward_Click(object sender, RoutedEventArgs e)
         {
             logTextBox.AppendText("Forward 10 seconds" + Environment.NewLine);
+            logTextBox.ScrollToEnd();
+        }
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            logTextBox.AppendText("Backwards 10 seconds" + Environment.NewLine);
+            logTextBox.ScrollToEnd();
         }
         private void About_Click(object sender, RoutedEventArgs e)
         {
             About about = new About();
             about.Show();
         }
+        private void Website_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.showtapeselection.com");
+        }
+        private void Discord_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://discord.gg/showtapeselection");
+        }
         private void Github_Click(object sender, RoutedEventArgs e)
         {
             System.Diagnostics.Process.Start("https://github.com/realrandombeans/Decoder-Studio");
         }
+
+        private void Record_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Coming soon.", "Record Mode", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+        private void ClearLog_Click(object sender, RoutedEventArgs e)
+        {
+            var Result = MessageBox.Show("Are you sure you wish to clear the log?", "Clear Log", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (Result == MessageBoxResult.Yes)
+            {
+                logTextBox.Clear();
+            }
+            else if (Result == MessageBoxResult.No)
+            {
+
+            }
+        }
+
+        private void Export_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Open a SaveFileDialog to choose the export location
+                SaveFileDialog saveFileDialog = new SaveFileDialog
+                {
+                    Title = "Export Log to Text File",
+                    DefaultExt = "txt",
+                    Filter = "Text Files (*.txt)|*.txt",
+                    FileName = "LogExport.txt"
+                };
+
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    // Get the selected file path from the SaveFileDialog
+                    string exportFilePath = saveFileDialog.FileName;
+
+                    // Write the contents of the logTextBox to the chosen file
+                    File.WriteAllText(exportFilePath, logTextBox.Text);
+
+                    // Show a success message
+                    MessageBox.Show("Log exported successfully.", "Export Complete", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Show an error message if export fails
+                MessageBox.Show($"An error occurred while exporting the log: {ex.Message}", "Export Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
     }
 }
